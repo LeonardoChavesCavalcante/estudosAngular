c4aa9c64f2e28c330bde7fec35e4ddaab1ee697e
Leonardo Chaves Cavalcante <leonardo.chavescavalcante@gmail.com>
2016-09-27 19:07:30 -0300

    Atividades aula3 Angular

--- HelloAngular2/src/app/meu-cadastro/meu-cadastro.component.ts
+++ HelloAngular2/src/app/meu-cadastro/meu-cadastro.component.ts
@@ -1,15 +1,25 @@
-import { Component } from "@angular/core";
+import { Component,OnInit,OnChanges,OnDestroy,DoCheck } from "@angular/core";
+
+import { Participante } from "./participante.model";
 
 @Component({
     selector:"meu-cadastro",
     templateUrl:"./meu-cadastro.component.html",
     styles:["./meu-cadastro.component.css"]
 }) 
-export class MeuCadastroComponent{
-    private resultado:String = "";
+export class MeuCadastroComponent implements OnInit{
+    private nome:String ="";
+    private participantes: Participante[] = [];
 
-    mostrar(){
-        
+    incluir(){
+       let participante: Participante = new Participante();
+       participante.nome = this.nome;
+       this.participantes.push(participante);              
     }
-
+    excluir(participante:Participante){        
+        this.participantes.splice(this.participantes.indexOf(participante),1);
+    }
+    ngOnInit(){
+        console.log("init");
+    }
 }

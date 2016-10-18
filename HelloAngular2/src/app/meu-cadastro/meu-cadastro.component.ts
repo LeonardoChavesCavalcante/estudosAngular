d9b54cf461933661befbfc61d2e2ff2ecc164108
Leonardo Chaves Cavalcante <leonardo.chavescavalcante@gmail.com>
2016-10-18 04:28:40 -0200

    atualizando conteudo

--- HelloAngular2/src/app/meu-cadastro/meu-cadastro.component.ts
+++ HelloAngular2/src/app/meu-cadastro/meu-cadastro.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnChanges, OnDestroy, DoCheck } from "@angular/core";
+import { Router } from '@angular/router';
 
 import { Participante } from "./participante.model";
 import {MeuCadastroService} from "./meu-cadastro.service";
 
 @Component({
+    moduleId: module.id,
     selector: "meu-cadastro",
     templateUrl: "./meu-cadastro.component.html",
     styles: ["./meu-cadastro.component.css"]
@@ -12,7 +14,8 @@ export class MeuCadastroComponent implements OnInit {
     private nome: String = "";
     private participantes: Participante[] = [];
 
-    constructor(private meuCadastroService: MeuCadastroService) {
+    constructor(private meuCadastroService: MeuCadastroService,
+                private router: Router) {
     }
 
     incluir() {
@@ -31,6 +34,11 @@ export class MeuCadastroComponent implements OnInit {
     buscar() {
         this.meuCadastroService.getParticipantes()
             .subscribe(resp => this.participantes = resp);
+    }
+
+    editar(){
+            this.router.navigate(["/detalhes"]);
+        
     }
     
     ngOnInit() {
--- HelloAngular2/src/app/meu-cadastro/meu-cadastro.module.ts
+++ HelloAngular2/src/app/meu-cadastro/meu-cadastro.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from "@angular/common";
 import { HttpModule } from "@angular/http";
+import {RouterModule,Routes } from "@angular/router";
 
 import { MeuCadastroComponent } from "./meu-cadastro.component";
 import { MeuCadastroService }  from "./meu-cadastro.service";
@@ -9,7 +10,11 @@ import { MeuCadastroDetalheComponent } from "./meu-cadastro-detalhe/meu-cadastro
 
 @NgModule({
   declarations: [MeuCadastroComponent ,MeuCadastroDetalheComponent ],
-  imports: [  FormsModule,CommonModule,HttpModule],
+  imports: [  FormsModule,CommonModule,HttpModule,
+            RouterModule.forRoot([{
+              path:"detalhes",
+              component: MeuCadastroDetalheComponent
+            }]) ],
   providers: [MeuCadastroService],
   exports:[MeuCadastroComponent,MeuCadastroDetalheComponent]
 })
